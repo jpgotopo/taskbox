59e18e2c3ceb955c811a9bfdca546a90b27960f0
Jpgotopo <jpgotopo@GMAIL.COM>
2021-01-12 15:14:05 -0500

    Incorporando Storybook al proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from './stories/Button'
+import {Header} from './stories/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +20,16 @@ function App() {
         >
           Learn React
         </a>
+          {/* <Button 
+            primary
+          
+          /> */}
+          <Button
+            backgroundColor="rgba(99,212,33,1)"
+            label="Entrar"
+            onClick={() => {}}
+            primary
+          />
       </header>
     </div>
   );
